e753d67f3eeb6f1fa820b2ee4876a5d7998f4c60
terodox@gmail.com <climbing1a>
2015-05-27 20:00:41 -0400

    Added basic solution setup with unit test project

---
+++ AzureStorageQueues.Tests/GivenAnAzureStorageQueue.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using NUnit.Framework;
+
+namespace AzureStorageQueues.Tests
+{
+	[TestFixture]
+	public class GivenAnAzureStorageQueue
+	{
+		[Test]
+		public void WhenAnAzureStorageQueueIsInstantiatedThenItDoesNotThrow()
+		{
+		}
+	}
+}
