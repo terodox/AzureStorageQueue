709544a060e1847cf281f335fa9a5e7a04205a77
terodox@gmail.com <climbing1a>
2015-06-21 19:59:50 -0400

    Tests now all pass. Need to add A LOT more

---
+++ Monitoring.Business.Tests/Helpers/MockMonitoringContext.cs
@@ -0,0 +1,63 @@
+﻿using Monitoring.Entities.Contracts;
+using Monitoring.Entities.Entities;
+using Moq;
+using System;
+using System.Collections.Generic;
+using System.Data.Entity;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Monitoring.Business.Tests.Helpers
+{
+	public static class MockMonitoringContextHelper
+	{
+		public static Mock<IMonitoringContext> MockMonitoringContext()
+		{
+			var mock = new Mock<IMonitoringContext>();
+			var mockDbSet = MockQueuesDbSet();
+
+			mock.SetupGet(mc => mc.Queues)
+				.Returns(mockDbSet.Object);
+
+			return mock;
+		}
+
+		public static Mock<IDbSet<Queue>> MockQueuesDbSet()
+		{
+			var queueQueryable = GenerateQueueEnumerable().AsQueryable();
+
+			return GenerateMockIDbSet(queueQueryable);
+		}
+
+		public static Mock<IDbSet<T>> GenerateMockIDbSet<T>(IQueryable<T> queryable) where T : class
+		{
+			var mock = new Mock<IDbSet<T>>();
+
+			mock.As<IQueryable<T>>().Setup(m => m.Provider)
+				.Returns(queryable.Provider);
+			mock.As<IQueryable<T>>().Setup(m => m.Expression)
+				.Returns(queryable.Expression);
+			mock.As<IQueryable<T>>().Setup(m => m.ElementType)
+				.Returns(queryable.ElementType);
+			mock.As<IQueryable<T>>().Setup(m => m.GetEnumerator())
+				.Returns(queryable.GetEnumerator());
+
+			return mock;
+		}
+
+		public static IEnumerable<Queue> GenerateQueueEnumerable(int count = 3, int startingIndex = 0)
+		{
+			return Enumerable.Range(startingIndex, count)
+				.Select(i => new Queue()
+				{
+					Created = new DateTime(2000, 1, 1, 0, 0, 0),
+					Updated = new DateTime(2000, 1, 1, 0, 0, 0),
+					Id = i,
+					ItemCount = i,
+					Name = "Fake Queue " + i,
+					Uri = "http://storage.queue" + i + ".com"
+				});
+		}
+	}
+}
---
+++ Monitoring.Business.Tests/Services/StorageQueueDatabaseLoggerTests.cs
@@ -0,0 +1,75 @@
+﻿using AzureStorageQueues.Contracts;
+using AzureStorageQueues.Entities;
+using Monitoring.Business.Services;
+using Monitoring.Business.Tests.Helpers;
+using Monitoring.Entities.Contracts;
+using Monitoring.Entities.Entities;
+using Moq;
+using NUnit.Framework;
+using System;
+using System.Collections.Generic;
+using System.Data.Entity;
+using System.Linq;
+using System.Text;
+
+namespace Monitoring.Business.Tests.Services
+{
+	/// <summary>
+	/// Summary description for StorageQueueDatabaseLoggerTests
+	/// </summary>
+	[TestFixture]
+	public class GivenAStorageQueueDatabaseLogger
+	{
+		private StorageQueueDatabaseLogger _sut;
+		private Mock<IAzureStorageQueueService> _azureStorageQueueServiceMock;
+		private Mock<IMonitoringContext> _monitoringContextMock;
+
+		[SetUp]
+		public void InitializeTest()
+		{
+			_azureStorageQueueServiceMock = MockAzureStorageQueueService();
+			_monitoringContextMock =MockMonitoringContextHelper.MockMonitoringContext();
+			_sut = new StorageQueueDatabaseLogger(_azureStorageQueueServiceMock.Object, _monitoringContextMock.Object);
+		}
+
+		[TearDown]
+		public void CleanupTest()
+		{
+			_sut = null;
+			_azureStorageQueueServiceMock = null;
+			_monitoringContextMock = null;
+		}
+
+		#region Setup Mocks
+		private Mock<IAzureStorageQueueService> MockAzureStorageQueueService()
+		{
+			var mock = new Mock<IAzureStorageQueueService>();
+
+			mock.Setup(asqs => asqs.GetStorageQueues())
+				.Returns(GenerateStorageQueueEnumerable().ToArray());
+
+			return mock;
+		}
+
+		private IEnumerable<StorageQueue> GenerateStorageQueueEnumerable(int count = 3, int startingIndex = 0)
+		{
+			return Enumerable.Range(startingIndex, count)
+				.Select(i => new StorageQueue(
+					"Storage Queue" + i,
+					new Uri("http://storage.queue" + i + ".com"),
+					new Dictionary<string, string>(),
+					i
+					));
+		}
+		#endregion
+
+		[Test]
+		public void WhenInstatiatedIDoNotThrow() { }
+
+		[Test]
+		public void WhenLogAllStorageQueuesToDatabaseThenDoNotThrow()
+		{
+			_sut.LogAllStorageQueuesToDatabase();
+		}
+	}
+}
